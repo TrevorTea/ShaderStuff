e68137858000405b02e7e66a0ca0c32ea78c7e83
Trevor <thomasjamestrevor@gmail.com>
2023-04-04 11:33:29 -0500

    Add webGL

--- GaussMouse/fragmentShader.js
+++ GaussMouse/fragmentShader.js
@@ -17,6 +17,36 @@ float timeOff(float offset) {
 }
 
 void main() {
-    vec3 rgb = vec3(timeOff(0.0), timeOff(1.0), timeOff(2.0)) * gaussMouse() * (sin(u_time * 2.0) + 1.0);
+    vec3 rgb = vec3(timeOff(0.0), timeOff(1.0), timeOff(2.0)) * gaussMouse();
 	gl_FragColor = vec4(rgb,1.0);
 }
+
+
+/**
+*WebGL implementation below
+
+float gaussMouse() {
+    vec2 dist = (gl_FragCoord.xy - iMouse.xy )/iResolution.xy;
+    float sigma = 0.5;
+	return 1.0 / (6.28 * pow(sigma, 2.0)) * exp(-(pow(dist[0], 2.0) + pow(dist[1], 2.0)) / 2.0 / pow(sigma, 2.0));
+}
+
+float timeOff(float offset) {
+    return sin(iTime * 2.0 + offset) + 1.0;
+}
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+    // Normalized pixel coordinates (from 0 to 1)
+    vec2 uv = fragCoord/iResolution.xy;
+
+    // Time varying pixel color
+    vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0,2,4));
+    
+    vec3 rgb = vec3(timeOff(0.0), timeOff(4.0), timeOff(2.0)) * gaussMouse();
+
+    // Output to screen
+    fragColor = vec4(rgb,1.0);
+}
+
+*/
